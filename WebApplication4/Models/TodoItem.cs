ce52b9a9f78f521c5ef4ae4e9df71de81d582096
Иваненко Влад <bombgamer532@gmail.com>
2024-07-27 16:28:33 +0300

    Initial commit

---
+++ WebApplication4/Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication4.Models
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+
+        public Guid Owner { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+    }
+}
